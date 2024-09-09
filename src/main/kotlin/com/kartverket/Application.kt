094436cb646f8f51d5b3e80e4e07c155be5a77ad
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-09-09 15:07:49 +0200

    cors

--- src/main/kotlin/com/kartverket/Application.kt
+++ src/main/kotlin/com/kartverket/Application.kt
@@ -1,6 +1,8 @@
 package com.kartverket
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -10,6 +12,12 @@ fun Application.module() {
     val config = environment.config
     Database.initDatabase(config)
     Database.migrate(config)
+
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.ContentType)
+    }
 
     configureSerialization()
     configureAuth()
