99384031fb099a706ebad5ec318cd5f00e96d604
Håkon Frøland <31542827+hakonfro@users.noreply.github.com>
2024-09-13 10:47:07 +0200

    Legger inn deploy til Fly.io med noen nødvendige endringer
    * La til ignore filer
    
    * Litt oppdrydning for å kjøre lokalt
    
    * Fikset kjøring i Docker
    
    * La inn Fly deploy
    
    * La til template for PR

--- src/main/kotlin/com/kartverket/Database.kt
+++ src/main/kotlin/com/kartverket/Database.kt
@@ -4,29 +4,39 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import java.sql.Connection
 import org.flywaydb.core.Flyway
-import io.ktor.server.config.ApplicationConfig
+import java.net.URI
 
 object Database {
     private lateinit var dataSource: HikariDataSource
 
-    fun initDatabase(config: ApplicationConfig) {
-        val jdbcUrl = config.property("ktor.database.jdbcURL").getString()
-        val jdbcUser = config.property("ktor.database.username").getString()
-        val jdbcPassword = config.property("ktor.database.password").getString()
-        val driver = config.property("ktor.database.driver").getString()
+    fun initDatabase() {
+        val env = System.getenv("environment")
+        val hikariConfig = HikariConfig()
+        if (env == "production") {
+            val databaseUrl = System.getenv("DATABASE_URL")
+            val dbUri = URI(databaseUrl)
+            val username = dbUri.userInfo.split(":")[0]
+            val password = dbUri.userInfo.split(":")[1]
+            val jdbcUrl = "jdbc:postgresql://${dbUri.host}:${dbUri.port}${dbUri.path}?sslmode=disable"
 
-        val hikariConfig = HikariConfig().apply {
-            this.jdbcUrl = jdbcUrl
-            username = jdbcUser
-            password = jdbcPassword
-            driverClassName = driver
+            hikariConfig.apply {
+                this.jdbcUrl = jdbcUrl
+                this.username = username
+                this.password = password
+                driverClassName = "org.postgresql.Driver"
+            }
+        } else {
+            // Local development environment
+            val jdbcUrl = "jdbc:postgresql://localhost:5432/frisk-backend-db"
+            val username = "postgres"
+            val password = ""
 
-            // Additional configuration
-            maximumPoolSize = 10
-            minimumIdle = 5
-            idleTimeout = 600000 // 10 minutes
-            connectionTimeout = 30000 // 30 seconds
-            maxLifetime = 1800000 // 30 minutes
+            hikariConfig.apply {
+                this.jdbcUrl = jdbcUrl
+                this.username = username
+                this.password = password
+                driverClassName = "org.postgresql.Driver"
+            }
         }
         dataSource = HikariDataSource(hikariConfig)
     }
@@ -40,22 +50,37 @@ object Database {
         dataSource.close()
     }
 
-    fun migrate(config: ApplicationConfig) {
-        val jdbcUrl = config.property("ktor.database.jdbcURL").getString()
-        val jdbcUser = config.property("ktor.database.username").getString()
-        val jdbcPassword = config.property("ktor.database.password").getString()
+    fun migrate() {
+        val env = System.getenv("environment")
+        val databaseUrl = System.getenv("DATABASE_URL")
+        val flywayConfig = Flyway.configure()
+        if (env == "production") {
+            // Production environment (Fly.io)
+            val dbUri = URI(databaseUrl)
+            val username = dbUri.userInfo.split(":")[0]
+            val password = dbUri.userInfo.split(":")[1]
+            val jdbcUrl = "jdbc:postgresql://${dbUri.host}:${dbUri.port}${dbUri.path}?sslmode=disable"
 
-        val flyway = Flyway.configure()
-            .dataSource(jdbcUrl, jdbcUser, jdbcPassword)
-            .locations("classpath:db/migration")
-            .load()
+            flywayConfig.dataSource(jdbcUrl, username, password)
+        } else {
+            // Local development environment
+            val jdbcUrl = "jdbc:postgresql://localhost:5432/frisk-backend-db"
+            val username = "postgres"
+            val password = "" 
+
+            flywayConfig.dataSource(jdbcUrl, username, password)
+        }
+        
+
+        flywayConfig.locations("classpath:db/migration")
+        val flyway = flywayConfig.load()
 
         val result = flyway.migrate()
         if (result.success) {
             println("Database migrations applied successfully.")
         } else {
             println("Failed to apply database migrations.")
-            // Here you might want to throw an exception or handle the failure appropriately
+            // Handle the failure appropriately
         }
     }
 }
