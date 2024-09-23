f4b61366a538ea9ef393676caeb820ca6e52a407
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-09-23 12:56:07 +0200

    fix auth

--- src/main/kotlin/com/kartverket/plugins/Auth.kt
+++ src/main/kotlin/com/kartverket/plugins/Auth.kt
@@ -10,15 +10,13 @@ import java.util.concurrent.TimeUnit
 val logger = KtorSimpleLogger("FunctionRoutes")
 
 fun Application.configureAuth() {
-    val baseUrl = System.getenv("baseUrl")
     val tenantId = System.getenv("tenantId")
-    val issuerPath = "/v2.0"
-    val issuer = "$baseUrl/$tenantId$issuerPath"
     val clientId = System.getenv("clientId")
-    val jwksPath = "/discovery/v2.0/keys"
-    val jwksUri = "$baseUrl/$tenantId$jwksPath"
+    val jwksUri = "https://login.microsoftonline.com/$tenantId/discovery/v2.0/keys"
+    val issuer = "https://login.microsoftonline.com/$tenantId/v2.0"
 
     logger.info("JWT URI: $jwksUri")
+    logger.info("issuer: $issuer")
 
     val jwkProvider =
         JwkProviderBuilder(URL(jwksUri))
@@ -28,22 +26,17 @@ fun Application.configureAuth() {
 
     install(Authentication) {
         jwt("auth-jwt") {
+            realm = "Frisk Backend"
             verifier(jwkProvider, issuer) {
-                logger.info("JWT issuer:  $issuer")
                 withIssuer(issuer)
-                acceptLeeway(3)
                 withAudience(clientId)
+                acceptLeeway(3)
+
             }
 
             validate { jwtCredential ->
-                logger.info("Validating JWT Credential: ${jwtCredential.payload}")
-                if (jwtCredential.audience.contains(clientId)) {
-                    logger.info("JWT Credential is valid")
-                    JWTPrincipal(jwtCredential.payload)
-                } else {
-                    logger.info("JWT Credential is invalid: Audience mismatch")
-                    null
-                }
+                logger.info("Validating JWT Credential: ${jwtCredential.payload.issuer}")
+                JWTPrincipal(jwtCredential.payload)
             }
         }
     }
