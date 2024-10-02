dac345820898055ccf1fea56cd86feb9b61ced35
Lars Sørensen <54811127+larsore@users.noreply.github.com>
2024-10-02 09:52:18 +0200

    Add workflow to deploy to skip (#12)
    * Add workflow to deploy to skip
    
    * update
    
    * update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update .github/workflows/skip-deploy.yml
    
    Co-authored-by: Johannes Bråthen Oma <johannesoma@gmail.com>
    
    ---------
    
    Co-authored-by: Johannes Bråthen Oma <johannesoma@gmail.com>

--- src/main/kotlin/com/kartverket/plugins/Routing.kt
+++ src/main/kotlin/com/kartverket/plugins/Routing.kt
@@ -1,12 +1,16 @@
 package com.kartverket.plugins
 
+import com.kartverket.Database
+import com.kartverket.functions.FunctionService
 import com.kartverket.functions.dependencies.functionDependenciesRoutes
 import com.kartverket.functions.functionRoutes
 import com.kartverket.functions.metadata.functionMetadataRoutes
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.routing.*
 import io.ktor.server.plugins.swagger.*
+import io.ktor.server.response.*
 
 fun Application.configureRouting() {
     routing {
@@ -17,6 +21,11 @@ fun Application.configureRouting() {
 
             // some of these routes shall be protected by another type of auth
             functionMetadataRoutes()
+        }
+        route("/functions") {
+            get("/health") {
+                call.respondText("Up and running!", ContentType.Text.Plain)
+            }
         }
     }
 }
