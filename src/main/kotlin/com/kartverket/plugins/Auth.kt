9722f5d95698855f6c0ab117213cccbc2c1e207f
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2024-10-03 14:32:50 +0200

    URL -> URI().toURL() (#19)

--- src/main/kotlin/com/kartverket/plugins/Auth.kt
+++ src/main/kotlin/com/kartverket/plugins/Auth.kt
@@ -4,7 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.util.logging.*
-import java.net.URL
+import java.net.URI
 import java.util.concurrent.TimeUnit
 
 const val AUTH_JWT = "auth-jwt"
@@ -21,7 +21,7 @@ fun Application.configureAuth() {
     logger.info("issuer: $issuer")
 
     val jwkProvider =
-        JwkProviderBuilder(URL(jwksUri))
+        JwkProviderBuilder(URI(jwksUri).toURL())
             .cached(10, 24, TimeUnit.HOURS)
             .rateLimited(10, 1, TimeUnit.MINUTES)
             .build()
