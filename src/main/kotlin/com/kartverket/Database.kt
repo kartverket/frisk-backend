45ac67a17fbfa44949fe585233645774269ea6cb
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-10-16 13:23:28 +0200

    fix

--- src/main/kotlin/com/kartverket/Database.kt
+++ src/main/kotlin/com/kartverket/Database.kt
@@ -15,7 +15,7 @@ object Database {
         val env = System.getenv("environment")
         val hikariConfig = HikariConfig()
         if (env == "production") {
-            HikariConfig().apply {
+            hikariConfig.apply {
                 val platform = System.getenv("platform")
                 when (platform) {
                     "flyio" -> {
