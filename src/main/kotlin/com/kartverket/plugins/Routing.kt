7c1a0c06384a1f13bc04b51cec31800532a0bdfd
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2024-12-16 15:21:51 +0100

    add dump endpoint for superusers (#66)

--- src/main/kotlin/com/kartverket/plugins/Routing.kt
+++ src/main/kotlin/com/kartverket/plugins/Routing.kt
@@ -1,8 +1,10 @@
 package com.kartverket.plugins
 
+import com.kartverket.Database
 import com.kartverket.functions.functionRoutes
 import com.kartverket.functions.metadata.functionMetadataRoutes
 import com.kartverket.microsoft.microsoftRoutes
+import com.kartverket.toCsv
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -17,6 +19,22 @@ fun Application.configureRouting() {
             functionRoutes()
             functionMetadataRoutes()
             microsoftRoutes()
+            get("/dump") {
+                if (!call.hasSuperUserAccess()) {
+                    call.respond(HttpStatusCode.Forbidden)
+                    return@get
+                }
+                val fileName = "data.csv"
+                call.response.header(
+                    HttpHeaders.ContentDisposition,
+                    ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, fileName).toString()
+                )
+
+                call.respondBytes(
+                    bytes = Database.getDump().toCsv().toByteArray(Charsets.UTF_8),
+                    contentType = ContentType.Text.CSV.withCharset(Charsets.UTF_8)
+                )
+            }
         }
         route("/health") {
             get {
