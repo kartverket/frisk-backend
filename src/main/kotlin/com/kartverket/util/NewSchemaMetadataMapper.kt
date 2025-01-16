e74847ff5d85496bc69ba2871068e5d419368762
malinnsnieske <malin.snieske@bekk.no>
2025-01-16 14:47:25 +0100

    lagt til logging i NewSchemaMetadataMapper (#77)
    Co-authored-by: Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>

--- src/main/kotlin/com/kartverket/util/NewSchemaMetadataMapper.kt
+++ src/main/kotlin/com/kartverket/util/NewSchemaMetadataMapper.kt
@@ -8,22 +8,28 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.util.logging.*
 import java.sql.ResultSet
 
 class NewSchemaMetadataMapper {
 
+    private val logger = KtorSimpleLogger("Database")
+
     suspend fun addNewSchemaMetadata() {
         val oldSchemaMetadata = getOldSchemaMetadata()
-
+        logger.info("Har hentet ut gamle skjema metadata")
         oldSchemaMetadata.forEach { m ->
             val contextId = m.value.split(":").first()
             val tableId = fetchTableId(contextId)
+            logger.info("Fikk hentet ut tableId")
 
             if (tableId != null) {
+                logger.info("Prøver å legge til ny skjema metadata")
                 addMetadataToFunction(
                     m.functionId,
                     CreateFunctionMetadataDTO(key = tableId, value = contextId)
                 )
+                logger.info("Lagt til ny skjema metadata")
             }
         }
     }
@@ -31,7 +37,7 @@ class NewSchemaMetadataMapper {
     fun getOldSchemaMetadata(): List<FunctionMetadata> {
         val query =
             "SELECT * FROM function_metadata fm INNER JOIN function_metadata_keys fmk ON fm.key_id = fmk.id WHERE fmk.key = 'rr-skjema'"
-
+        logger.info("Henter gamle skjema metadata")
         return Database.getConnection().use { connection ->
             connection.prepareStatement(query).use { statement ->
                 statement.executeQuery().use { resultSet ->
@@ -56,17 +62,21 @@ class NewSchemaMetadataMapper {
                 }
                 else -> "https://regelrett-frontend-1024826672490.europe-north1.run.app/api"
             }
-
+            logger.info("Kaller på regelrett")
             val response: HttpResponse =
                 client.get("$baseUrl/contexts/$contextId/tableId")
             if (response.status == HttpStatusCode.OK) {
+                logger.info("Fått status ok fra regelrett")
                 response.bodyAsText()
             } else {
+                logger.info("Feil ved henting av tableId fra regelrett")
                 null
             }
         } catch (e: Exception) {
+            logger.info("Feilmelding ble catchet i fetchTableId ${e.message}")
             null
         } finally {
+            logger.info("All good i fetchTableId, lukker klienten")
             client.close()
         }
     }
