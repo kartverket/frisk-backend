a653dfdea60d74430166645ae25e6cbd5a8414e3
gyda <gyda1210@hotmail.com>
2025-02-07 09:42:42 +0100

    401 test added

--- src/test/kotlin/com/kartverket/ApplicationTest.kt
+++ src/test/kotlin/com/kartverket/ApplicationTest.kt
@@ -1,3 +1,8 @@
+package com.kartverket
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
@@ -5,4 +10,29 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
     }
+    @Test
+    fun testHealth() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/health")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Up and running!", response.bodyAsText())
+    }
+
+    @Test
+    fun testAuhorisationRequiredInEndpoints() = testApplication {
+        application {
+            module()
+        }
+
+        val endpoints = listOf("/functions", "metadata", "/microsoft/me/teams", "/dump")
+        endpoints.forEach {
+            val response = client.get(it)
+            assertEquals(HttpStatusCode.Unauthorized, response.status)
+        }
+    }
+
+
+
 }
