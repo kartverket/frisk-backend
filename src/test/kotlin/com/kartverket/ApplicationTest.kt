08fa124043b4a2e85aa56fdadd2a27f565659dc0
Thomas Oddsund <thomas.oddsund@bekk.no>
2025-03-06 10:36:10 +0100

    Add general auth test
    This test is simply to verify that the Authentication plugin is enabled
    on all routes not explicitly allowlisted to be public, when starting the
    application using the application module function.
    Any explicit handling of the authentication(e.g. access via
    roles/id/team etc) is left to test for each route.

--- src/test/kotlin/com/kartverket/ApplicationTest.kt
+++ src/test/kotlin/com/kartverket/ApplicationTest.kt
@@ -1,8 +1,56 @@
+import com.kartverket.Database
+import com.kartverket.module
+import io.ktor.server.routing.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import io.mockk.every
+import io.mockk.mockkObject
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.fail
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
+    fun `Verify that authentication is enabled on non-public endpoints`() = testApplication {
+        application {
+            // Mock the Database object with stubs to avoid actual DB initialization and migrations during the test
+            mockkObject(Database) {
+                every { Database.initDatabase() } returns Unit
+                every { Database.migrate() } returns Unit
+                module()
+                routing {
+                    val publicEndpointsRegexList = listOf(
+                        Regex("^/swagger"),
+                        Regex("^/health")
+                    )
+
+                    // Get all registered routes and filter out those that match any of the public endpoint regex patterns
+                    val nonPublicRoutes = getAllRoutes().filter { route ->
+                        publicEndpointsRegexList.none { regex ->
+                            regex.containsMatchIn(route.toString())
+                        }
+                    }
+
+                    assertAll("Authentication in routes", nonPublicRoutes.map { route ->
+                        // The `assertionForRoute@` is a label to enable us to return from the function if we find
+                        // the Authenticate plugin.
+                        assertionForRoute@ {
+                            var currRoute: Route? = route
+                            // The Authenticate plugin that we are looking for is possibly defined earlier in
+                            // the route hierarchy, so we traverse upwards via the parent property.
+                            while (currRoute != null) {
+                                // Checks if the Authenticate plugin is enabled in the current routes pipeline
+                                if (currRoute.items.any { it.name == "Authenticate" }) {
+                                    return@assertionForRoute
+                                }
+                                currRoute = currRoute.parent
+                            }
+
+                            fail("$route does not have authentication enabled")
+                        }
+                    }
+                    )
+                }
+            }
+        }
     }
 }
