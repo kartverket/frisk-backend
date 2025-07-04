6a91279ee3f27982b422d88e698559d29f5e7d3e
Jørn Ola Birkeland <73480629+jorn-ola-birkeland@users.noreply.github.com>
2025-07-04 11:33:46 +0200

    Bumper en del versjoner (#218)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,16 @@ val logback_version: String by project
 val postgres_version: String by project
 val h2_version: String by project
 val exposed_version: String by project
+val testcontainers_version = "1.21.3"
+val mockk_version = "1.14.3"
+val flyway_version = "11.10.0"
+val microsoft_graph_version = "6.42.1"
 
 plugins {
     kotlin("jvm") version "2.1.21"
     id("io.ktor.plugin") version "3.1.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
-    id("org.flywaydb.flyway") version "11.8.2"
+    id("org.flywaydb.flyway") version "11.10.0"
     id("com.gradleup.shadow") version "8.3.6"
 }
 
@@ -43,8 +47,8 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("com.zaxxer:HikariCP:6.3.0")
-    implementation("org.flywaydb:flyway-core:11.8.2")
-    implementation("org.flywaydb:flyway-database-postgresql:11.8.2")
+    implementation("org.flywaydb:flyway-core:$flyway_version")
+    implementation("org.flywaydb:flyway-database-postgresql:$flyway_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
@@ -53,15 +57,15 @@ dependencies {
     implementation("io.ktor:ktor-server-swagger:$ktor_version")
     implementation("com.azure:azure-identity:1.+")
     implementation("net.minidev:json-smart:2.5.2") /* kan slettes når Azure oppdaterer denne selv*/
-    implementation("com.microsoft.graph:microsoft-graph:6.39.0")
+    implementation("com.microsoft.graph:microsoft-graph:$microsoft_graph_version")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-cio-jvm:$ktor_version")
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
-    testImplementation("io.mockk:mockk:1.14.2")
-    testImplementation("org.testcontainers:testcontainers:1.21.0")
-    testImplementation("org.testcontainers:postgresql:1.21.0")
+    testImplementation("io.mockk:mockk:$mockk_version")
+    testImplementation("org.testcontainers:testcontainers:$testcontainers_version")
+    testImplementation("org.testcontainers:postgresql:$testcontainers_version")
 }
 
 flyway {
